325c2fda56a3a1d8e59d2b88120e42ee6090c4b2
Alexander Klimenko <klime.98@mail.ru>
2018-12-22 22:48:07 +0300

    some refactoring for extending it to other architectures

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import random
+import time
+import math
+import matplotlib.pyplot as plt
+
+def someFunc():
+    global z
+    z = 1
+
+someFunc()
+print(z)
